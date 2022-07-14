fd48b212a4754547e07149257f781c5f978f4dd3
Nikita Tikhonov <nekitdevofficial@gmail.com>
2022-07-14 19:26:40 +0300

    Update main line.

--- melody/web/tailwind.config.js
+++ melody/web/tailwind.config.js
@@ -1,4 +1,12 @@
 module.exports = {
     content: ["./**/*.html"],
     darkMode: "media",
+    theme: {
+        extend: {
+            colors: {
+                "melody-purple": "#cc55ff",
+                "melody-blue": "#55ccff"
+            },
+        }
+    }
 }
