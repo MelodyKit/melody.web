9ed665e38c1f62e28391842a8c54e64eb5a9d87e
Nikita Tikhonov <nekitdevofficial@gmail.com>
2025-02-09 23:11:00 +0300

    Bump dependencies, remove `deno` usage.

--- build.rs
+++ build.rs
@@ -1,9 +1,20 @@
 use std::process::Command;
 
-pub const DENO: &str = "deno";
-pub const TASK: &str = "task";
-pub const BUILD: &str = "build";
+pub const CSS: &str = "tailwindcss";
+pub const INPUT: &str = "--input";
+pub const OUTPUT: &str = "--output";
+pub const MINIFY: &str = "--minify";
+
+pub const INPUT_PATH: &str = "static/css/input.css";
+pub const OUTPUT_PATH: &str = "static/css/output.css";
 
 fn main() {
-    Command::new(DENO).arg(TASK).arg(BUILD).status().unwrap();
+    Command::new(CSS)
+        .arg(INPUT)
+        .arg(INPUT_PATH)
+        .arg(OUTPUT)
+        .arg(OUTPUT_PATH)
+        .arg(MINIFY)
+        .status()
+        .unwrap();
 }
